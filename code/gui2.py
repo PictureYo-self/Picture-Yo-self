9870be6c8c2fabbe8ecb3755c1034873275ffd15
Samir <ss67@rice.edu>
2015-10-29 16:53:09 -0500

    Create gui2.py

---
+++ code/gui2.py
@@ -0,0 +1,62 @@
+from random import random
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+from kivy.graphics import Color, Ellipse, Line
+from kivy.app import App
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.lang import Builder
+
+Builder.load_string(’’’
+<CustomLayout>
+  canvas.before:
+    Color:
+      rgba: 0, 1, 0, 1
+    Rectangle:
+      pos: self.pos
+      size: self.size
+      
+<RootWidget>
+  CustomLayout:
+    AsyncImage:
+      source: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Lovett_Hall.jpg'
+      #size_hint: 1, .5
+      #pos_hint: {'center_x':.5, 'center_y': .5}
+
+class MyPaintWidget(Widget):
+	
+	def on_touch_down(self, touch):
+		color = (random(), 1, 1)
+		with self.canvas:
+			Color(*color, mode='hsv')
+			d = 30.
+			#Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=(d, d))
+			touch.ud['line'] = Line(points=(touch.x, touch.y))
+
+	def on_touch_move(self, touch):
+		touch.ud['line'].points += [touch.x, touch.y]
+
+class RootWidget(BoxLayout):
+  pass
+class CustomLayout(FloatLayout):
+  pass
+
+class MyPaintApp(App):
+
+	def build(self):
+		parent = Widget()
+		painter = MyPaintWidget()
+		clearbtn = Button(text='Clear')
+		parent.add_widget(painter)
+		parent.add_widget(clearbtn)
+
+		def clear_canvas(obj):
+			painter.canvas.clear()
+		clearbtn.bind(on_release=clear_canvas)
+
+		return parent
+
+
+if __name__ == '__main__':
+	MyPaintApp().run()
