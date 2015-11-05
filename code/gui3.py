e6d0a5ca64a1a2a62c16682ed8ce25142e24f1f0
momo137 <momo137@users.noreply.github.com>
2015-11-05 13:47:25 -0600

    fix tabs

--- code/gui3.py
+++ code/gui3.py
@@ -8,30 +8,30 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.lang import Builder
 
-root = Builder.load_string('''
-  FloatLayout:
-      canvas.before:
-          Color:
-              rgb: 1, 1, 1
-          Rectangle:
-              source: '/home/pi/Picture-Yo-self/code/pictures/ci3.jpg'
-              size: self.size
-  BoxLayout:
-        padding: 10
-        spacing: 10
-        size_hint: 1, None
-        pos_hint: {'top': 1}
-        height: 44
-        Image:
-            size_hint: None, None
-            size: 24, 24
+#root = Builder.load_string('''
+FloatLayout:
+  canvas.before:
+    Color:
+      rgb: 1, 1, 1
+        Rectangle:
             source: '/home/pi/Picture-Yo-self/code/pictures/ci3.jpg'
+            size: self.size
+BoxLayout:
+      padding: 10
+      spacing: 10
+      size_hint: 1, None
+      pos_hint: {'top': 1}
+      height: 44
+      Image:
+          size_hint: None, None
+          size: 24, 24
+          source: '/home/pi/Picture-Yo-self/code/pictures/ci3.jpg'
 #        Label:
 #            height: 24
 #            text_size: self.width, None
 #            color: (1, 1, 1, .8)
 #            text: 'Kivy %s - Pictures' % kivy.__version__
-''')
+#''')
 
 
 class RootWidget(BoxLayout):
