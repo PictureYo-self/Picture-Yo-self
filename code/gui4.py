31241b8680bc4c6635bb064965cf3ad93e5ccb45
momo137 <momo137@users.noreply.github.com>
2015-11-05 13:59:05 -0600

    add in drawing feature maybe

--- code/gui4.py
+++ code/gui4.py
@@ -2,7 +2,11 @@ from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.lang import Builder
-
+from random import random
+from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+from kivy.graphics import Color, Ellipse, Line
+from kivy.app import App
 
 Builder.load_string('''
 <CustomLayout>
@@ -14,30 +18,44 @@ Builder.load_string('''
             size: self.size
 
 <RootWidget>
-    #CustomLayout:
-    #    AsyncImage:
-    #        source: '/home/pi/Picture-Yo-self/code/pictures/ci3.jpg'
-    #        size_hint: 1, .5
-    #        pos_hint: {'center_x':.5, 'center_y': .5}
     AsyncImage:
         source: '/home/pi/Picture-Yo-self/code/pictures/ss67.jpg'
-    #CustomLayout
-    #    AsyncImage:
-    #        source: '/home/pi/Picture-Yo-self/code/pictures/jt18.jpg'
-    #        size_hint: 1, .5
-    #        pos_hint: {'center_x':.5, 'center_y': .5}
 ''')
+
+class MyPaintWidget(Widget):
+	
+	def on_touch_down(self, touch):
+		color = (random(), 1, 1)
+		with self.canvas:
+			Color(*color, mode='hsv')
+			d = 30.
+			#Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=(d, d))
+			touch.ud['line'] = Line(points=(touch.x, touch.y))
+
+	def on_touch_move(self, touch):
+		touch.ud['line'].points += [touch.x, touch.y]
+		
+class MainApp(App):
+
+	def build(self):
+	    return RootWidget()
+		parent = Widget()
+		painter = MyPaintWidget()
+		clearbtn = Button(text='Clear')
+		parent.add_widget(painter)
+		parent.add_widget(clearbtn)
+
+		def clear_canvas(obj):
+			painter.canvas.clear()
+		clearbtn.bind(on_release=clear_canvas)
+
+		return parent
 
 class RootWidget(BoxLayout):
     pass
 
 class CustomLayout(FloatLayout):
     pass
-
-class MainApp(App):
-
-    def build(self):
-        return RootWidget()
 
 if __name__ == '__main__':
     MainApp().run()
