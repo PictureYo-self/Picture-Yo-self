248f6d73229acf5feadc886970a486a9a238ab99
momo137 <momo137@users.noreply.github.com>
2015-11-05 16:07:22 -0600

    add import image

--- code/editName.py
+++ code/editName.py
@@ -6,7 +6,7 @@ from random import random
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.graphics import Color, Ellipse, Line
-from kivy.app import App
+from kivy.uix.image import Image
 
 f = open('/home/pi/Picture-Yo-self/code/pictures/picName.txt','r')
 global picname 
