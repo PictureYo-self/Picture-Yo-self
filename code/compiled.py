8823072eb8d23bdd6dd44f1c7b3780f58cf9a700
momo137 <momo137@users.noreply.github.com>
2015-11-05 15:40:48 -0600

    Update compiled.py

--- code/compiled.py
+++ code/compiled.py
@@ -25,7 +25,6 @@ Builder.load_string('''
     	pos: self.pos
     	size: self.size
 ''')
-'''
 class MyPaintWidget(Widget):
 	def on_touch_down(self, touch):
 		color = (random(), 1, 1)
@@ -61,4 +60,3 @@ class CustomLayout(FloatLayout):
 
 if __name__ == '__main__':
     MainApp().run()
-'''
