71655d3f8b5ac2e06667206f650f92a028680857
momo137 <momo137@users.noreply.github.com>
2015-11-05 14:47:41 -0600

    Update compiled.py

--- code/compiled.py
+++ code/compiled.py
@@ -21,6 +21,8 @@ class MyPaintWidget(Widget):
 		color = (random(), 1, 1)
 		with self.canvas:
 			Color(*color, mode='hsv')
+			d = 30.
+			#Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=(d, d))
 			touch.ud['line'] = Line(points=(touch.x, touch.y))
 
 	def on_touch_move(self, touch):
