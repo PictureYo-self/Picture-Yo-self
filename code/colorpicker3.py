9a1b984aea218910ade997d2b096c0b5f69175ac
Momona Yamagami <momo137@users.noreply.github.com>
2015-11-19 13:44:15 -0600

    Update colorpicker3.py

--- code/colorpicker3.py
+++ code/colorpicker3.py
@@ -1,56 +1,34 @@
-from kivy.app import App
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.boxlayout import BoxLayout
-from kivy.lang import Builder
-from random import random
+from kivy.uix.popup import Popup
 from kivy.uix.widget import Widget
-from kivy.graphics import Color, Rectangle
-from kivy.uix.button import Button
-from kivy.graphics import Color, Ellipse, Line
-from kivy.uix.image import Image
 from kivy.uix.colorpicker import ColorPicker
+from kivy.app import App
+from kivy.properties import ListProperty
+col = [0,0,1,1]
 
-f = open('/home/pi/Picture-Yo-self/code/pictures/picName.txt','r')
-global picname 
-picname = f.read()
-f.close()
+class SelectedColorEllipse(Widget):
+	selected_color = ListProperty(col)
 
-class MyPaintWidget(Widget):
-	def on_touch_down(self, touch):
-		color = (0, 1, 1, 1)
-		with self.canvas:
-			Color(*color)
-			touch.ud['line'] = Line(points=(touch.x, touch.y), width=5)
+class ColPckr(ColorPicker):
+	pass
 
-	def on_touch_move(self, touch):
-		touch.ud['line'].points += [touch.x, touch.y]
+class ColPopup(Popup):
+	pass
+    
+class Ex40(Widget):
+	selected_color = ListProperty(col)
+	def select_ColPckr(self,*args):
+        	ColPopup().open()
+    	def on_touch_down(self, touch):
+        	if touch.x <100 and touch.y < 100:
+            		return super(Ex40, self).on_touch_down(touch)
+        	sce = SelectedColorEllipse()
+        	sce.selected_color = self.selected_color
+        	sce.center = touch.pos
+        	self.add_widget(sce)
+          
+class Ex40App(App):
+    	def build(self):
+        	return Ex40()
 
-class MainApp(App):
-	im=Image(source=picname, size_hint=(1,50))
-	def build(self):
-		root = BoxLayout(orientation='vertical')
-		#c = Imglayout()
-		parent = BoxLayout(orientation='horizontal')
-		painter = MyPaintWidget()
-		clr_picker = ColorPicker()
-		clr_picker.bind(color=on_color)
-		clearbtn = Button(text='Clear', size_hint=(1,5))
-		savebtn = Button(text='Save', size_hint=(1,5))
-
-		root.add_widget(self.im)
-		parent.add_widget(clearbtn)
-		parent.add_widget(savebtn)
-		root.add_widget(painter)
-		root.add_widget(clr_picker)
-		def clear_canvas(obj):
-			painter.canvas.clear()
-		clearbtn.bind(on_release=clear_canvas)
-		#root.add_widget(c)
-		root.add_widget(parent)
-		return root
-		
-class RootWidget(BoxLayout):
-    pass
-
-if __name__ == '__main__':
-    MainApp().run()
+if __name__=='__main__':
+    	Ex40App().run()
