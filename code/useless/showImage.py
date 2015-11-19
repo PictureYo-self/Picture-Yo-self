266bb4081c436172cfc1bcb03eaa2a78830f466f
Picture Yo-self <ss67@rice.edu>
2015-11-19 01:47:57 -0600

    move some files around

---
+++ code/useless/showImage.py
@@ -0,0 +1,58 @@
+from kivy.app import App
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.lang import Builder
+from random import random
+from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+from kivy.graphics import Color, Ellipse, Line
+from kivy.app import App
+
+Builder.load_string('''
+<CustomLayout>
+    canvas.before:
+        Color:
+            rgba: 0, 1, 0, 1
+        Rectangle:
+            pos: self.pos
+            size: self.size
+
+<RootWidget>
+    AsyncImage:
+        source: '/home/pi/Picture-Yo-self/code/pictures/ss67.jpg'
+''')
+
+class MyPaintWidget(Widget):
+	def on_touch_down(self, touch):
+		color = (random(), 1, 1)
+		with self.canvas:
+			Color(*color, mode='hsv')
+			d = 30.
+			#Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=(d, d))
+			touch.ud['line'] = Line(points=(touch.x, touch.y))
+
+	def on_touch_move(self, touch):
+		touch.ud['line'].points += [touch.x, touch.y]
+		
+class MainApp(App):
+	def build(self):
+		return RootWidget()
+		parent = Widget()
+		painter = MyPaintWidget()
+		clearbtn = Button(text='Clear')
+		parent.add_widget(painter)
+		parent.add_widget(clearbtn)
+		def clear_canvas(obj):
+			painter.canvas.clear()
+		clearbtn.bind(on_release=clear_canvas)
+
+		return parent
+
+class RootWidget(BoxLayout):
+    pass
+
+class CustomLayout(FloatLayout):
+    pass
+
+if __name__ == '__main__':
+    MainApp().run()
---
+++ code/useless/working.py
@@ -0,0 +1,81 @@
+from kivy.app import App
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.lang import Builder
+from random import random
+from kivy.uix.widget import Widget
+from kivy.graphics import Color, Rectangle
+from kivy.uix.button import Button
+from kivy.graphics import Color, Ellipse, Line
+from kivy.uix.image import Image
+import sys
+
+f = open('/home/pi/Picture-Yo-self/code/pictures/picName.txt','r')
+global picname 
+picname = f.read()
+f.close()
+
+f = open('/home/pi/Picture-Yo-self/code/pictures/email.txt','r')
+email = f.read()
+f.close()
+email = '/home/pi/Picture-Yo-self/code/pictures' + email + '.png'
+
+class MyPaintWidget(Widget):
+	def on_touch_down(self, touch):
+		color = (random(), 1, 1)
+		with self.canvas:
+			Color(*color, mode='hsv')
+			touch.ud['line'] = Line(points=(touch.x, touch.y))
+
+	def on_touch_move(self, touch):
+		touch.ud['line'].points += [touch.x, touch.y]
+		
+class Imglayout(FloatLayout):
+
+    def __init__(self,**args):
+        super(Imglayout,self).__init__(**args)
+
+        with self.canvas.before:
+            Color(0,0,0,0)
+            self.rect=Rectangle(size=self.size,pos=self.pos)
+
+        self.bind(size=self.updates,pos=self.updates)
+    def updates(self,instance,value):
+        self.rect.size=instance.size
+        self.rect.pos=instance.pos
+		
+class MainApp(App):
+	im=Image(source=picname, size_hint=(1,50))
+	def build(self):
+		root = BoxLayout(orientation='vertical')
+		c = Imglayout()
+		parent = BoxLayout(orientation='horizontal')
+		painter = MyPaintWidget()
+
+		# create clear button and save button
+		clearbtn = Button(text='Clear')#, size_hint=(1,5))
+		parent.add_widget(clearbtn)
+		def clear_canvas(obj):
+			painter.canvas.clear()
+		clearbtn.bind(on_release=clear_canvas)
+		
+		savebtn = Button(text='Save')#, size_hint=(1,5))
+		def save_pic(obj):
+			#root.remove_widget(parent)
+			root.export_to_png(email)
+			app.Exit()
+		savebtn.bind(on_release=save_pic)
+		root.add_widget(self.im)
+		root.add_widget(painter)
+		root.add_widget(c)
+		root.add_widget(parent)
+		return root
+		
+class RootWidget(BoxLayout):
+    pass
+
+class CustomLayout(FloatLayout):
+    pass
+
+if __name__ == '__main__':
+    MainApp().run()
