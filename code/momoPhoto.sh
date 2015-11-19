efed2616a2e14089c1eb1c73ea8ddf1bbac5bf9d
Momona Yamagami <momo137@users.noreply.github.com>
2015-11-19 01:59:13 -0600

    Update momoPhoto.sh

--- code/momoPhoto.sh
+++ code/momoPhoto.sh
@@ -5,15 +5,25 @@ filename=`cat ./pictures/picName.txt`
 
 netid=`cat ./pictures/email.txt`
 
+email1=$netid"@rice.edu"
+name=`finger $email1 | grep "name" | awk '{print $3}'`
+grad=`finger $email1 | grep "class" | awk '{print $2}'`
+college=`finger $email1 | grep "college" | awk '{print $2}'`
+
+if ($grad=='graduate')
+  {
+  college='graduate'
+  }
+
+$college > college.txt
+
 python screenManager.py
 
 filename="/home/pi/Picture-Yo-self/code/pictures/"$netid".png"
 
-email1=$netid"@rice.edu"
-name=`finger $email1 | grep "name" | awk '{print $3}'`
-college=`finger $email1 | grep "college" | awk '{print $2}'`
 
-print $college
+
+
 
 mpack -s "Thanks, $name, for using Rice Photobooth!" -d ./pictures/body.txt $filename $email1
 
