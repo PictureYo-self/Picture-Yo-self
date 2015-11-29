4d8c2dc2210968189c03d5cb3a15d7f2629d6a96
Picture Yo-self <ss67@rice.edu>
2015-11-29 14:50:17 -0600

    updated colorpicker

--- code/colorpicker10.py
+++ code/colorpicker10.py
@@ -1,9 +1,5 @@
 #// screen manager imported from http://kivy.org/docs/api-kivy.uix.screenmanager.html
 
-from kivy.uix.popup import Popup
-from kivy.uix.colorpicker import ColorPicker
-from kivy.properties import ListProperty
-from kivy.properties import BooleanProperty
 from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.boxlayout import BoxLayout
@@ -17,58 +13,26 @@ from kivy.uix.image import Image
 import sys
 from kivy.clock import Clock
 
-
 f = open('/home/pi/Picture-Yo-self/code/pictures/picName.txt','r')
 picname = f.read()
 f.close()
-
-
-f = open('/home/pi/Picture-Yo-self/code/pictures/picName2.txt','r')
-picname2 = f.read()
-f.close()
+print picname
 
 f = open('/home/pi/Picture-Yo-self/code/pictures/email.txt','r')
 email = f.read()
 f.close()
 email = '/home/pi/Picture-Yo-self/code/pictures/' + email + '.png'
 
-f = open('/home/pi/Picture-Yo-self/code/pictures/picName2.txt','w')
-f.write(email)
-f.close()
-
 f = open('/home/pi/Picture-Yo-self/code/college.txt','r')
-college = f.read()
+col = f.readline().strip()
 f.close()
 
-crest='/home/pi/Picture-Yo-self/code/pictures/' + college
+college = '/home/pi/Picture-Yo-self/code/pictures/' + col
+print col
+#college = '/home/pi/Picture-Yo-self/code/pictures/Jones.jpg'#' + col + '.jpg' 
+print college
 
-
-
-col = [0,0,1,1]
-# define colorpicker classes
-class SelectedColorEllipse(Widget):
-	selected_color = ListProperty(col)
-
-class ColPckr(ColorPicker):
-	pass
-
-class ColPopup(Popup):
-	pass
-
-class Ex40(Widget):
-    	selected_color = ListProperty(col)
-    	def select_ColPckr(self,*args):
-        	ColPopup().open()
-	def on_touch_down(self, touch):
-        	if touch.x <100 and touch.y < 100:
-            		return super(Ex40, self).on_touch_down(touch)
-        	sce = SelectedColorEllipse()
-        	sce.selected_color = self.selected_color
-        	sce.center = touch.pos
-        	self.add_widget(sce)
-
-'''
-# define painter class
+#reload(sys)
 class MyPaintWidget(Widget):
 	def on_touch_down(self, touch):
 		color = (random(), 1, 1)
@@ -78,19 +42,16 @@ class MyPaintWidget(Widget):
 
 	def on_touch_move(self, touch):
 		touch.ud['line'].points += [touch.x, touch.y]
-'''
-
-class Ex40App(App):
-#	im1=Image(source=picname, size_hint=(1,50))
-#	allow_stretch = BooleanProperty(True)
-#	im2=Image(source='/home/pi/Picture-Yo-self/code/pictures/Will.jpg') #,size=(10,5000))
+		
+class MainApp(App):
+	im=Image(source=picname, size_hint=(1,50))
+	crest=Image(source=college, size_hint=(25,25))#, pos=(1,1))
 	def build(self):
 		root = BoxLayout(orientation='vertical')
-#		parent = BoxLayout(orientation='horizontal')
-		#painter = MyPaintWidget()
-		painter = Ex40()
+		parent = BoxLayout(orientation='horizontal')
+		painter = MyPaintWidget()
+		crestwid = BoxLayout(orientation='horizontal')
 		
-		'''
 		# create clear button
 		clearbtn = Button(text='Clear', size_hint=(1,5))
 		parent.add_widget(clearbtn)
@@ -111,26 +72,23 @@ class Ex40App(App):
 		savebtn = Button(text='Save and send to email', size_hint=(1,5))
 		parent.add_widget(savebtn)
 		def save_pic(obj):
-			root.remove_widget(parent)
-			root.export_to_png(email)
-			os.remove(picname)
-			#os.remove(picname2)
-			exit()
+		  parent.remove_widget(savebtn)
+		  parent.remove_widget(clearbtn)
+		  parent.remove_widget(retakebtn)
+		  root.export_to_png(email)
+		  exit()
 		savebtn.bind(on_release=save_pic)
-		'''
-		
-#		root.add_widget(self.im1) 
-#		root.add_widget(self.im2)
-		painter.canvas.ask_update()
-#		root.add_widget(painter)
-#		root.add_widget(parent)
-		#root.painter.canvas.ask_update()		
-		return Ex40()
+
+		crestwid.add_widget(self.crest)
+		parent.add_widget(crestwid)
+		root.add_widget(self.im)
+		root.add_widget(painter)
+		root.add_widget(parent)		
+		#root.add_widget(crestwid)
 		return root
-		return Ex40()
-		
+	
 class RootWidget(BoxLayout):
     pass
 
 if __name__ == '__main__':
-    Ex40App().run()
+    MainApp().run()
